445ab4bb41c270fead5f5ca8a21dea475e2e1abb
Rafael Paez <civic.vtieg6@outlook.com>
2025-03-08 19:08:51 -0300

    ranges

---
+++ pycharm/exercicios/aula09.py
@@ -0,0 +1,17 @@
+frase = 'Curso em video python'
+print(frase[3:13])
+print(frase[13:])
+print(frase[1:15:2])
+print(frase.upper().count('o'))
+print(len(frase.strip()))
+print(frase.replace('python', 'android'))
+print(frase)
+print('Curso' in frase)
+print(frase.lower().find('Curso'))
+divido = frase.split()
+print(divido[2][3])
+
+
+
+print("""texto bem grande texto bem grande texto bem grande texto bem grande texto bem grande texto bem grande
+texto bem grande texto bem grande texto bem grande""")
