5edfd5c42a8c1e0a98bd05933ee07c890d64c503
Rafael Paez <rafaelantonucci.vti@gmail.com>
2025-04-19 19:09:10 -0300

    emprestimo

--- desafios/emprestimo.py
+++ desafios/emprestimo.py
@@ -1,1 +1,13 @@
-print('===')
+print('='*20)
+print('EMPRÉSTIMO')
+print('='*20)
+casa = float(input('Qual o valor da casa em R$: '))
+tempo = int(input('Quantos anos de financiamento? '))
+salario = float(input('Qual o seu sálario '))
+prestação = casa / (tempo * 12)
+minimo = salario * 30/100
+print('Para pagar uma casa de R${} a prestacao sera de {:.2f}'.format(casa, prestação))
+if prestação <= minimo:
+    print('Emprestimo aprovado')
+else:
+    print('Negado! ')
